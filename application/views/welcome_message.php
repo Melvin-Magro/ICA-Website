b62028cbff861c2364ee3afd08e32d520fafce65
FrannyCarabez <37878322+FrannyCarabez@users.noreply.github.com>
2018-04-14 18:40:50 +0200

    Added Logo

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -12,6 +12,7 @@
 
     <!-- Bootstrap core CSS -->
     <link href="<?php echo base_url('vendor/bootstrap/css/bootstrap.min.css'); ?>" rel="stylesheet">
+    <link href="<?php echo base_url('vendor/bootstrap/css/extra.css'); ?>" rel="stylesheet">
 
     <!-- Custom fonts for this template -->
     <link href="<?php echo base_url('vendor/font-awesome/css/font-awesome.min.css'); ?>" rel="stylesheet" type="text/css">
@@ -31,7 +32,9 @@
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
       <div class="container">
-        <a class="navbar-brand js-scroll-trigger" href="<?php echo base_url();?>#page-top">Mcast</a>
+        <a class="navbar-brand js-scroll-trigger" href="<?php echo base_url();?>#page-top">
+          <img class="img-logo" src="img/portfolio/thumbnails/mainLogo.png" alt="">
+        </a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
