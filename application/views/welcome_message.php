acc1877fb49064c8b57d892a0db46bea7789a205
rejxil <36443591+rejxil@users.noreply.github.com>
2018-04-20 14:44:44 +0200

    Footer links

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -296,10 +296,10 @@
     <div class="row">
       <div class="col-md-3">
         <ul>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Clients</a></li>
-          <li><a href="#">Contact Us</a></li>
+          <li><a href="<?php echo base_url();?>#about">About</a></li>
+          <li><a href="<?php echo base_url();?>#courses">Courses</a></li>
+          <li><a href="<?php echo base_url();?>#portfolio">Portfolio</a></li>
+          <li><a href="<?php echo base_url();?>#contact">Contact</a></li>
         </ul>
       </div>
       <div class="col-md-3">
