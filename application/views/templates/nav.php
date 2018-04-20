b9650d3a6d4fd14d63f28f0e664eb2d90bcc39ec
FrannyCarabez <37878322+FrannyCarabez@users.noreply.github.com>
2018-04-20 23:31:44 +0200

    Logo changed and it invertes

--- application/views/templates/nav.php
+++ application/views/templates/nav.php
@@ -4,7 +4,7 @@
     <nav class="navbar navbar-expand-lg navbar-light fixed-top<?=($shrink) ? "" : " do-not-shrink" ?>" id="mainNav">
       <div class="container">
         <a class="navbar-brand js-scroll-trigger" href="<?php echo base_url();?>#page-top">
-          <img class="img-logo" src="<?=base_url('img/mainLogo.png')?>" alt="">
+          <img class="img-logo" src="<?=base_url('img/mainLogoWhite.png')?>" alt="">
         </a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
